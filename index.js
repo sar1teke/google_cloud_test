d9a9199b361014b506e1583f83ce4d8b4035b0b2
ismail sariteke <issariteke@hotmail.com>
2025-03-13 10:30:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// PostgreSQL Bağlantısı
+const pool = new Pool({
+  user: 'ismail',
+  host: '34.59.3.14', // Cloud IP adresiniz
+  database: 'phoenix',
+  password: '1',
+  port: 5432,
+});
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Ana sayfa form gösterme
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM bilgiler', (err, result) => {
+    if (err) {
+      return res.send('Verileri çekerken hata oluştu: ' + err);
+    }
+    res.render('index', { veriler: result.rows });
+  });
+});
+
+// Formdan gelen veriyi ekleme
+app.post('/ekle', (req, res) => {
+  const { isim, sehir } = req.body;
+  pool.query('INSERT INTO bilgiler (isim, sehir) VALUES ($1, $2)', [isim, sehir], (err) => {
+    if (err) {
+      return res.send('Kayıt sırasında hata oluştu: ' + err);
+    }
+    res.redirect('/');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${port}`);
+});
